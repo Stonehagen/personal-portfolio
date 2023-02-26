9827973ecf138b0ecf5ab6e76ef60371dfa959c6
Stonehagen <steinhagen@mailbox.org>
2023-02-26 14:49:39 +0100

    add about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import '../styles/About.css'
+import '../styles/About.css';
+import ProfilePic from '../img/profile.jpg';
 
 const About = () => {
   return (
     <div className="About">
-      <h1>About Me</h1>
+      <img src={ProfilePic} alt='Tobias Steinhagen'/>
+      <h1>About me</h1>
+      <p>
+        My name is Tobias Steinhagen. I'm a self tough full-stack web developer
+        based in Luebeck, Germany. I am also a educated mechanical engineer.
+      </p>
     </div>
   );
 };
