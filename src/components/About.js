7ea145775ba0f2b0972968449d2576a558dec63f
Stonehagen <steinhagen@mailbox.org>
2023-04-12 22:25:27 +0200

    change text in about

--- src/components/About.js
+++ src/components/About.js
@@ -12,18 +12,16 @@ const About = () => {
         based in Luebeck, Germany. I am also a educated mechanical engineer.
       </p>
       <p>
-        I have always had a deep passion for coding, and one year ago, I made a
-        personal commitment to pursue a career as a Web Developer. I have
-        diligently honed my coding skills through various reputable sources,
-        such as Harvard's CS50 program, FreeCodeCamp, The Odin Project, and
-        self-study with books like "You Don't Know JS." Additionally, I have
-        undertaken numerous personal projects to further strengthen my
-        expertise.
+        I always had a deep passion for coding, and one year ago, I made a
+        personal commitment to pursue a career as a Web Developer. I have earned
+        my coding skills through various sources, such as Harvard's CS50
+        program, FreeCodeCamp, The Odin Project, and self-study with books like
+        "You Don't Know JS." Additionally, I have undertaken numerous personal
+        projects to further strengthen my expertise.
       </p>
       <p>
         All of my portfolio projects have been developed using the MERN stack.
-        So please scroll down and explore these portfolio projects, allowing me
-        to reaffirm my skills and expertise in web development.
+        So please scroll down and explore my projects.
       </p>
     </div>
   );
