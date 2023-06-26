81c294a1b3a5875ca997fe045f469af090be6b35
Stonehagen <steinhagen@mailbox.org>
2023-06-26 21:04:26 +0200

    add light mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import About from './components/About';
 import Contact from './components/Contact';
 import Header from './components/Header';
 import Projects from './components/Projects';
+import Menu from './components/Menu';
 
 const App = () => {
+  const [lightMode, setLightMode] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle('lightmode', lightMode);
+  }, [lightMode]);
+
   return (
     <div className="App">
+      <Menu lightMode={lightMode} setLightMode={setLightMode} />
       <Header />
       <About />
       <Projects />
