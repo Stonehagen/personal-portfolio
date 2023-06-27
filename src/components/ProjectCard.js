f676c5155f5779173c9c75156b363dd8322057b8
Stonehagen <steinhagen@mailbox.org>
2023-06-27 21:51:52 +0200

    add responsive design for projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,24 +4,32 @@ import '../styles/ProjectCard.css';
 const ProjectCard = ({ project }) => {
   return (
     <div className="ProjectCard">
-      <div className="ProjectCard-Image">
-        <img src={project.img} alt={project.name} />
+      <div className="ProjectCard-Info">
+        <div className="ProjectCard-Image">
+          <img src={project.img} alt={project.name} />
+        </div>
+        <div className="ProjectCard-Text">
+          <h2>{project.name}</h2>
+          <p>{project.text}</p>
+          <ul>
+            {project.srcLinks.map((link, index) => {
+              return (
+                <li key={index}>
+                  <a href={link}>{`Repo ${index + 1}`}</a>
+                </li>
+              );
+            })}
+            <li>
+              <a href={project.liveLink}>Live</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="ProjectCard-Text">
-        <h2>{project.name}</h2>
-        <p>{project.text}</p>
-        <ul>
-          {project.srcLinks.map((link, index) => {
-            return (
-              <li key={index}>
-                <a href={link}>{`Repo ${index + 1}`}</a>
-              </li>
-            );
-          })}
-          <li>
-            <a href={project.liveLink}>Live</a>
-          </li>
-        </ul>
+      <div className="ProjectCard-Web">
+          <img src={project.web} alt={project.name} />
+      </div>
+      <div className="ProjectCard-Phone">
+          <img src={project.phone} alt={project.name} />
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import '../styles/Projects.css';
 import Project1 from '../img/projects/Project1_1.png';
+import Project1Phone from '../img/projects/Project1_Phone.png';
+import Project1Web from '../img/projects/Project1_Web.png';
 import Project2 from '../img/projects/Project2_1.png';
+import Project2Phone from '../img/projects/Project2_Phone.png';
+import Project2Web from '../img/projects/Project2_Web.png';
 import ProjectCard from './ProjectCard';
 
 const Projects = () => {
@@ -10,6 +14,8 @@ const Projects = () => {
       name: 'Social Network',
       text: 'A Social Network with 2000 fake profiles. You have to create a profile to get access to the network or use the test account. Its possible to make private or global posts, make friend requests, send private messages and many more. To facilitate these functionalities, I have developed an Express backend API with socketIO for the chat function that interacts with a MongoDB database. The fully responsible frontend is a React application that makes API calls for all CRUD (Create, Read, Update, Delete) operations.',
       img: Project2,
+      phone: Project2Phone,
+      web: Project2Web,
       srcLinks: [
         'https://github.com/Stonehagen/social-network-backend',
         'https://github.com/Stonehagen/social-network-frontend',
@@ -20,6 +26,8 @@ const Projects = () => {
       name: 'Blog',
       text: 'A fictitious travel blog featuring AI-generated posts, where users can sign up to create, edit, publish, and delete their own posts. Additionally, visitors can leave comments on posts without registering, and authors have the ability to delete comments. To facilitate these functionalities, I have developed an Express backend API that interacts with a MongoDB database. The frontend is a React application that makes API calls for all CRUD (Create, Read, Update, Delete) operations.',
       img: Project1,
+      phone: Project1Phone,
+      web: Project1Web,
       srcLinks: [
         'https://github.com/Stonehagen/odin-blog-api-backend',
         'https://github.com/Stonehagen/odin-blog-frontend',
