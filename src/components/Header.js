2b8bbb907255b882371148aeda9dc63445d49a1e
Stonehagen <steinhagen@mailbox.org>
2023-06-28 11:09:58 +0200

    add terminal to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../styles/Header.css';
 import { ReactComponent as Cursor } from '../img/cursor.svg';
+import { ReactComponent as Terminal } from '../img/terminal.svg';
 
 const Header = () => {
   return (
     <div className="Header">
       <div>
+        <Terminal className="Terminal" />
         <h1>
           Hi, I'm <span>Tobias</span>
         </h1>
         <h2>
           A <span>FULL-STACK</span> WEB DEVELOPER
-          <Cursor />
+          <Cursor className="Cursor" />
         </h2>
       </div>
     </div>
